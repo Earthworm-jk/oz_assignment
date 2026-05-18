9db88b2b19535f6419c3590914508245df7cdd45
Earthworm-jk <nkimjeeyoung@gmail.com>
2026-05-18 11:43:59 +0900

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,3 @@
 print("Hello, World!")
+print("This is a simple Python program that prints 'Hello, World!' to the console. It serves as a basic example of how to write and run a Python script.")
+print("You can run this program using a Python interpreter, and it will display the message on the screen. This is often the first program that beginners write when learning a new programming language.") 
